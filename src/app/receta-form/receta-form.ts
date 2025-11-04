d7b601bddbddd1c0bcbcd514f5eec3bdcdd2aa20
Ivan <ivancarrillovela@gmail.com>
2025-11-04 10:28:00 +0100

    Implementar formulario para añadir nuevas recetas con validaciones y manejo de ingredientes

--- src/app/receta-form/receta-form.ts
+++ src/app/receta-form/receta-form.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RecetaModel } from '../models/RecetaModel';
+
+type DatosReceta = Omit<RecetaModel, 'id'>;
 
 @Component({
   selector: 'app-receta-form',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './receta-form.html',
-  styleUrl: './receta-form.scss',
+  styleUrl: './receta-form.scss'
 })
-export class RecetaForm {
+export class RecetaFormComponent {
 
+  agregarReceta = output<DatosReceta>();
+  
+  formularioReceta = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
+    urlImagen: new FormControl('https://images.unsplash.com/photo-1546069901-ba9599a7e63c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDg4NDh8MHwxfGFsbHwxfHx8fHx8fHwxNjE2MTg0OTI4&ixlib-rb-1.2.1&q=80&w=400', [Validators.required]),
+    ingredientes: new FormControl('', [Validators.required])
+  });
+
+  get nombre() {
+    return this.formularioReceta.get('nombre');
+  }
+
+  get urlImagen() {
+    return this.formularioReceta.get('urlImagen');
+  }
+
+  get ingredientes() {
+    return this.formularioReceta.get('ingredientes');
+  }
+
+  enviarFormulario() {
+    if (this.formularioReceta.invalid) {
+      return;
+    }
+
+    const arrayIngredientes = this.formularioReceta.value.ingredientes!
+      .split('\n')
+      .filter((line: string) => line.trim().length > 0);
+
+    const valorFormulario = this.formularioReceta.value;
+
+    const nuevaReceta: DatosReceta = {
+      nombre: valorFormulario.nombre!,
+      urlImagen: valorFormulario.urlImagen!,
+      ingredientes: arrayIngredientes,
+    };
+    
+    this.agregarReceta.emit(nuevaReceta);
+
+    this.formularioReceta.reset();
+  }
 }
