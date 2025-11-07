f19bcf54fe6d22a083b95a013e7d8d68deffbb70
Ivan <ivancarrillovela@gmail.com>
2025-11-07 18:09:36 +0100

    Añadido componente Footer a la aplicación, incluyendo su lógica y plantilla. Se ha actualizado app.html para incluir el nuevo componente.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Recetas } from './recetas/recetas';
 import { Header } from "./header/header";
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Recetas, Header],
+  imports: [Recetas, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
