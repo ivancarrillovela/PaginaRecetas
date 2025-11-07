208f05624653ac6cdbd24417e67985858588aaf4
Ivan <ivancarrillovela@gmail.com>
2025-11-07 17:02:51 +0100

    Añadido el componente Header en la estructura de la aplicación y actualizado el archivo app.html para incluirlo.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Recetas } from './recetas/recetas';
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [Recetas],
+  imports: [Recetas, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
