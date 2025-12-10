731ca3a672008d9df5943995927c67e27cdabad9
Ivan <ivancarrillovela@gmail.com>
2025-12-10 12:55:20 +0100

    Configurar rutas incluyendo vista de detalle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Recetas } from './recetas/recetas';
+import { RecetaDetalleComponent } from './receta-detalle/receta-detalle';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Recetas },
+  { path: 'receta/:id', component: RecetaDetalleComponent }, // Ruta con parámetro
+  { path: '**', redirectTo: '' }
+];
