3602e8c07c11e4fc52113cb525ff86c2ee44e835
Ivan <ivancarrillovela@gmail.com>
2025-12-10 11:15:41 +0100

    Integrar valoración y navegación al detalle en RecetaCard

--- src/app/receta-card/receta-card.ts
+++ src/app/receta-card/receta-card.ts
@@ -1,20 +1,25 @@
 import { Component, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router'; 
 import { RecetaModel } from '../models/RecetaModel';
+import { StarRatingComponent } from '../star-rating/star-rating';
 
 @Component({
   selector: 'app-receta-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, StarRatingComponent],
   templateUrl: './receta-card.html',
   styleUrl: './receta-card.scss'
 })
 export class RecetaCard {
-
-  receta = input<RecetaModel>();
+  receta = input.required<RecetaModel>();
   borrarReceta = output<string>();
+  votarReceta = output<number>(); // Nuevo Output para el voto
 
   alBorrar() {
-    this.borrarReceta.emit(this.receta()!.nombre);
+    this.borrarReceta.emit(this.receta().id);
   }
 
+  alVotar(puntuacion: number) {
+    this.votarReceta.emit(puntuacion);
+  }
 }
