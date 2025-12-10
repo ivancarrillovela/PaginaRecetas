392a453a371368fc6fc5eb759dfdf3da0548ad81
Ivan <ivancarrillovela@gmail.com>
2025-12-10 18:35:42 +0100

    Actualizar puntuaciones de recetas en db.json y mejorar lógica de filtrado en el componente Recetas para que filtre por el numero de estrellas exacto

--- src/app/recetas/recetas.ts
+++ src/app/recetas/recetas.ts
@@ -1,8 +1,10 @@
 import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule, AsyncPipe } from '@angular/common';
 import { BehaviorSubject, combineLatest, map, Observable, startWith, switchMap, Subscription } from 'rxjs';
+
 import { RecetaModel } from '../models/RecetaModel';
 import { RecetasService } from '../services/recetas.service';
+
 import { RecetaCard } from '../receta-card/receta-card';
 import { RecetaForm } from '../receta-form/receta-form';
 import { Filtro } from '../filtro/filtro';
@@ -31,14 +33,14 @@ export class Recetas implements OnInit, OnDestroy {
   private filtroNombre$ = new BehaviorSubject<string>('');
   private filtroEstrellas$ = new BehaviorSubject<number>(0);
 
-  // ViewModel (Datos combinados)
+  // ViewModel
   vm$!: Observable<{ recetas: RecetaModel[], filtroActual: string }>;
 
   // Estado de la Vista
   recetaSeleccionadaId: string | null = null;
   mostrarFormulario = false; 
 
-  // Variable para gestionar la suscripción (Teoría pág. 65)
+  // Gestión de suscripciones
   private formSubscription?: Subscription;
 
   ngOnInit() {
@@ -51,21 +53,26 @@ export class Recetas implements OnInit, OnDestroy {
     // Combinación con filtros
     this.vm$ = combineLatest([datosServidor$, this.filtroNombre$, this.filtroEstrellas$]).pipe(
       map(([recetas, nombre, estrellas]) => {
-        const filtradas = recetas.filter(r => 
-          r.nombre.toLowerCase().includes(nombre) && r.puntuacion >= estrellas
-        );
+        const filtradas = recetas.filter(r => {
+          const coincideNombre = r.nombre.toLowerCase().includes(nombre);
+          
+          // LÓGICA CAMBIADA: Filtro exacto
+          // Si estrellas es 0, mostramos todas.
+          // Si no, comparamos la parte entera de la puntuación (4.8 -> 4) con el filtro.
+          const coincideEstrellas = estrellas === 0 || Math.floor(r.puntuacion) === estrellas;
+
+          return coincideNombre && coincideEstrellas;
+        });
         return { recetas: filtradas, filtroActual: nombre };
       })
     );
 
     // Suscripción al estado global del formulario
-    // Guardamos la suscripción en la variable
     this.formSubscription = this.recetasService.formOpen$.subscribe(isOpen => {
       this.mostrarFormulario = isOpen;
     });
   }
 
-  // Limpiamos la suscripción al destruir el componente
   ngOnDestroy() {
     if (this.formSubscription) {
       this.formSubscription.unsubscribe();
